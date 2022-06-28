200de35ea695e2af261a28a661efcf137433198a
Samya Ahsan <96796200+samyaahsan@users.noreply.github.com>
2022-06-27 22:44:39 -0400

    Time: 0 ms (100.00%), Space: 43.5 MB (21.13%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -15,19 +15,11 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        // base case
-        // the root we are at is null, so its depth is 0
-        if(root == null) {
+        if (root == null)
             return 0;
-        }
         
-        // recursive case
-        // we are at a non null node so return 1 (for our current node)
-        // + max of the left subtree depth and the right subtree depth
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        return 1 + Math.max(leftDepth, rightDepth);
-        //return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-        
+        int left = 1 + maxDepth(root.left);
+        int right = 1 + maxDepth(root.right);
+        return Math.max(left,right);
     }
 }
