de6e82ea166d767ca5b74db73c05e4385f389343
Samya Ahsan <96796200+samyaahsan@users.noreply.github.com>
2022-11-16 15:18:31 -0500

    Time: 717 ms (95.60%), Space: 18.1 MB (41.10%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,14 +4,12 @@ class Solution:
         nums.sort()
         
         for i, curr in enumerate(nums):
-            #i is the index
-            #curr is the current element
             if i > 0 and curr == nums[i-1]:
                 continue
                 
-            l, r = i + 1 , len(nums) - 1
-            #left is i + 1 because curr is the first element, so we want to get the rest
-            while l < r:
+            l, r = i + 1, len(nums) - 1
+            
+            while (l < r):
                 three_sum = curr + nums[l] + nums[r]
                 if three_sum > 0:
                     r -= 1
@@ -19,10 +17,9 @@ class Solution:
                     l += 1
                 else:
                     res.append([curr, nums[l], nums[r]])
-                    l += 1 #keep counter moving, only have to handle one case
-                    while nums[l-1] == nums[l] and l < r: #get rid of duplicates
+                    l += 1
+                    while l < r and nums[l-1] == nums[l]:
                         l += 1
-            
-        return res
-         
         
+        return res
+                
