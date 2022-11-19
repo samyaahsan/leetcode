bf6ecc30ccf41d64998fb4265016b8d284b9524d
Samya Ahsan <96796200+samyaahsan@users.noreply.github.com>
2022-11-19 18:36:45 -0500

    Time: 179 ms (22.30%), Space: 13.9 MB (66.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,24 +2,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def dfs(i, curr_combo, total):
-            #base cases
-            if total == target: #success
-                res.append(curr_combo.copy())
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
                 return
-            if i >= len(candidates) or total > target: #failure
+            if i >= len(candidates) or total > target:
                 return
             
-            #branching
-            #include
-            curr_combo.append(candidates[i])
-            dfs(i, curr_combo, total + candidates[i])
-            curr_combo.pop()
+            curr.append(candidates[i])
+            dfs(i, curr, total+candidates[i])
+            curr.pop()
             
-            #exclude
-            dfs(i+1, curr_combo, total)
+            dfs(i+1, curr, total)
             
         dfs(0, [], 0)
         return res
-                
-        
